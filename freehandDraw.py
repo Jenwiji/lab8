23c97dad19579688acca95bec41b35c38e151082
MingNitcha <99862394+MingNitcha@users.noreply.github.com>
2022-02-24 18:35:35 +0700

    commit

---
+++ freehandDraw.py
@@ -0,0 +1,58 @@
+import sys
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+class freeHandDraw(QLabel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.white)
+        self.setAutoFillBackground(True)
+        self.setPalette(p)
+        self.myPixmap = QPixmap(200,200)
+        self.setMinimumSize(200,200)
+        self.painter = QPainter(self.myPixmap)
+        self.pen = QPen(Qt.black)
+        self.painter.setPen(self.pen)
+        self.painter.fillRect(0,0,200,200, Qt.white)
+        self.setPixmap(self.myPixmap)
+        self.last = None
+
+    def mouseMoveEvent(self, event):
+        if self.last:
+            self.painter.drawLine(self.last, event.pos())
+
+            self.last = event.pos()
+            self.setPixmap(self.myPixmap)
+            self.update()
+
+    def mousePressEvent(self, event):
+        self.last = event.pos()
+
+    def mouseReleaseEvent(self, event):
+        self.last = None
+
+    def updateSize(self, width, height):
+        pm = QPixmap(width, height)
+        pm.fill(Qt.white)
+        old = self.myPixmap
+        self.myPixmap = pm
+        self.pen = QPen(Qt.black)
+        self.painter = QPainter(pm)
+        self.painter.drawPixmap(0,0,old)
+        self.setPixmap(pm)
+
+    def resizeEvent(self, event):
+        if event.oldSize().width() > 0:
+            self.updateSize(event.size().width(), event.size().height())
+
+def main():
+    app = QApplication(sys.argv)
+    w = freeHandDraw()
+    w.show()
+
+    return app.exec()
+
+if __name__ == "__main__":
+    sys.exit(main())
